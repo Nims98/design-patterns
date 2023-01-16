e0ad01ab62f23e215d881be06e2b4e4220cd57d7
Nims98 <nirmalasabaragamuwa@gmail.com>
2023-01-16 20:29:17 +0530

    encapsulation

--- src/com/nims/Main.java
+++ src/com/nims/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // loose coupling
         TaxCalculator calculator = new TaxCalculator1();
+        // encapsulation
+        Account account = new Account();
+        account.deposit(20);
+        account.withdraw(5);
+        System.out.println(account.getBalance());
     }
     }
